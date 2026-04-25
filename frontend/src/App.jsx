8e2337db848d56e0ed9ad0ba5004ca441e70cc05
Vedant-07 <vedantnpatel7@gmail.com>
2026-04-25 22:22:16 +0530

    refactored frontend code

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <p className='text-5xl text-amber-300'>hi world</p>
+      <Button variant="outline">Button</Button>
+    </>
+  )
+}
+
+export default App
